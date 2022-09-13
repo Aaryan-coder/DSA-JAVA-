a18d99ca870f8e77d1d79b0ec56c301f4a8c6a1c
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2022-09-13 18:13:29 +0530

    Create SwapAlternate.java

---
+++ Array/SwapAlternate.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+    
+    public static void swapAlternate(int arr[]) {
+    	int temp;
+        for(int i = 0;i<arr.length-1;i+=2){
+            temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+}
