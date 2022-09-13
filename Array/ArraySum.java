6ffea9b29f7b5e039d461f94cc9d9ad0c86a5114
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2022-09-13 17:23:48 +0530

    Update ArraySum.java

--- Array/ArraySum.java
+++ Array/ArraySum.java
@@ -1,1 +1,11 @@
-
+import java.util.*;
+public class Solution {
+	
+	public static int sum(int[] arr) {
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+	}
+}
