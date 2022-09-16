e44751675f3cd3bd4cb33e829f24ba7f27bdc037
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2022-09-16 19:45:21 +0530

    Create WaveArray.java

---
+++ Company/Microsoft/WaveArray.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public static void convertToWave(int n, int[] arr) {
+        for(int i = 0;i<n-1;i++){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+            i++;
+        }
+    }
+}
+ 
