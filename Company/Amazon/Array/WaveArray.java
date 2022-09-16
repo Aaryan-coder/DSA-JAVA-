0256927d06ff1fa78119aec2ea4a6836be466213
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2022-09-16 19:44:06 +0530

    Update WaveArray.java

--- Company/Amazon/Array/WaveArray.java
+++ Company/Amazon/Array/WaveArray.java
@@ -1,29 +1,12 @@
-static void Rotate(int arr[], int d, int n)
-{
-    // Storing rotated version of array
-    int temp[] = new int[n];
- 
-    // Keepig track of the current index
-    // of temp[]
-    int k = 0;
- 
-    // Storing the n - d elements of
-    // array arr[] to the front of temp[]
-    for (int i = d; i < n; i++) {
-        temp[k] = arr[i];
-        k++;
-    }
- 
-    // Storing the first d elements of array arr[]
-    //  into temp
-    for (int i = 0; i < d; i++) {
-        temp[k] = arr[i];
-        k++;
-    }
- 
-    // Copying the elements of temp[] in arr[]
-    // to get the final rotated array
-    for (int i = 0; i < n; i++) {
-        arr[i] = temp[i];
+
+class Solution {
+    public static void convertToWave(int n, int[] arr) {
+        for(int i = 0;i<n-1;i++){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+            i++;
+        }
     }
 }
+ 
